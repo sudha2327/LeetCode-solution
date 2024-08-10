3998fdd2a00a0c596dc604488a9b0104297509d3
Sudhagar_vetrivel <59435694+sudha2327@users.noreply.github.com>
2024-08-10 19:58:49 +0530

    Time: 51 ms (77.28%), Space: 16.5 MB (69.5%) - LeetHub

---
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -0,0 +1,39 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        a = l1 #pointers
+        b = l2 #pointers
+        arr1 = []
+        arr2 = []
+
+        while a:
+          arr1.append(a.val)
+          a = a.next
+
+        while b:
+          arr2.append(b.val)
+          b = b.next
+
+        arr1.reverse()
+        arr2.reverse()
+
+        inta = int("".join(str(x) for x in arr1)) #converting list to strings
+        intb = int("".join(str(x) for x in arr2))
+
+        c = list(str(inta + intb)) #performing addition - the answer we wanted
+
+        # assign last digit to new ListNode which represents the head of returned LL
+        head = l3 = ListNode(c.pop())
+
+        c.reverse()
+
+        # traverse remaining digits, assigning each to new ListNode
+        for i in c:
+            l3.next = ListNode(i)
+            l3 = l3.next
+
+        return head 
